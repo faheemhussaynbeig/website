b6d344c56241f641fb80cef7d38dcfb75d97e410
faheemhussaynbeig <119043543+faheemhussaynbeig@users.noreply.github.com>
2022-12-06 12:06:24 +0530

    Add files via upload
    Start button added

--- index.js
+++ index.js
@@ -1,35 +1,51 @@
-var level=0;
-var move_number=0;
+// Required Variables
+var highscore=0;
+var clicker;
+var level;
+var move_number;
 var moves=[];
 var bts=["green","red","yellow","blue"];
-
 start();
 
+// Game resetter
 function start(){
-level=0;
-move_number=0;
-moves=[];
-bts=["green","red","yellow","blue"];
-$(document).on("keypress",function(){
-    $(document).unbind("keypress");
-    play();
-});
+    clicker=false;
+    level=0;
+    move_number=0;
+    moves=[];
+
 }
 
+// Only allow key press when level is 0
+$(".btn").on("click",function(){
+    if(!level){
+        $(".front").hide();
+        play();
+    }
+});
+
+// Click listener for buttons
+$("button").on("click",function(){
+    if(clicker)
+        check(this.id);
+}); 
+
+// Level up after every right pattern
 function levelUp(){
     $("h1").text("Level "+ (++level));
 }
 
+// Play computer pattern
 function play(){
     levelUp();
     moves.push(Math.floor(Math.random()*4));
     for(var i=0;i<moves.length;i++)
         AnimC(i);
-    $("button").on("click",function(){
-        check(this.id);
-    });    
+    clicker=true;
+       
 }
 
+// Computer Animation with delay between button presses
 function AnimC(i){
     setTimeout(function(){
     (new Audio("sounds/"+bts[moves[i]]+".mp3")).play();
@@ -39,6 +55,7 @@ function AnimC(i){
     },400*i);
 }
 
+// User button Animation with no delay between button presses
 function AnimH(i){
     (new Audio("sounds/"+bts[moves[i]]+".mp3")).play();
     $("#"+bts[moves[i]]).addClass("flash");
@@ -46,21 +63,24 @@ function AnimH(i){
         document.querySelector("#"+bts[moves[i]]).classList.remove("flash")},200);
 }
 
+// Checks if user click is right
 function check(id){
     if(bts[moves[move_number]]==id){
-            AnimH(move_number++);
-            if(move_number==moves.length){
-                move_number=0;
-                $("button").off("click");
-                setTimeout(function(){
-                    play();
+            AnimH(move_number++);           //Animate current press
+            if(move_number==moves.length){  //If all buttons are pressed right
+                if(level>highscore)         //Check for High Score
+                    $("#highscore").text("Highscore: "+(++highscore));
+                clicker=false;             //Disable button after sequence ends
+                move_number=0;              //reset move index
+                setTimeout(function(){      //Add delay between user move and next computer pattern
+                    play();                 //Call play function for next sequence
                 },900);
             }
         }
-        else{
+        else{                               //If any button is wrongly pressed, stop game.
             (new Audio("sounds/wrong.mp3")).play();
-            $("button").off("click");
-            $("h1").text("Game Over! Press any key to restart.");
+            $("h1").text("Game Over! Press Start to play again. Highscore: "+(level-1));
+            $(".front").show();
             start();
         }
 }
